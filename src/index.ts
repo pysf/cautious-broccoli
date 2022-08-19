6b505ab86487534b65dddd938c6826b7c88a6be4
pysf <payam.ysfi@gmail.com>
2022-08-19 14:20:31 +0200

    chore: add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 app.listen(4000, () => {
-	console.log('server running on port 4000');
-});
+    console.log('server running on port 4000')
+    console.log('oeu')
+})
